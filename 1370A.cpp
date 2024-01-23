c5a8f14e2598f5d73110f1250a86a158a36a3281
InvertedPallete <itachi52730@gmail.com>
2024-01-23 23:03:39 +0530

    new

---
+++ 1370A.cpp
@@ -0,0 +1,17 @@
+//
+//  23/01/2024
+//  
+//  @itachi52730
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
