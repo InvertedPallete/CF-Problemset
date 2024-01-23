a931e213e51a99f0962a8829e07757fcc902c526
InvertedPallete <itachi52730@gmail.com>
2024-01-23 22:29:11 +0530

    Final submmited

--- 1624A.cpp
+++ 1624A.cpp
@@ -11,7 +11,29 @@ int main()
     cin>>t;
     while(t--)
     {
-        
+        int n;
+        cin>>n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+        }
+
+        sort (a,a+n);
+        int max = a[n-1];
+
+        for(int i = 0; i<n ; i++ )
+        {
+            a[i] = max - a[i];
+
+            if(a[i] < 0)
+            {
+                a[i] = a[i] * (-1);
+            }
+        }        
+
+        sort(a,a+n);
+        cout<< a[n-1] << endl; 
     }
     return 0;
 }
