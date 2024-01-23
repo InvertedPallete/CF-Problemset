b56e78f4510bee27a65dce39936d0f9209fbbeca
InvertedPallete <itachi52730@gmail.com>
2024-01-23 22:21:45 +0530

    Changes template

--- 1624A.cpp
+++ 1624A.cpp
@@ -1,17 +1,8 @@
-//
-//  23/01/2024
-//  
-//  @itachi52730
-//
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int t;
     cin>>t;
-    while(t--)
-    {
-        
-    }
     return 0;
 }
