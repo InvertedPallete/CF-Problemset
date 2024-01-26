80acf992f751e916c1e11e89d67f75a8cf537d17
InvertedPallete <itachi52730@gmail.com>
2024-01-26 21:38:55 +0530

    edited

---
+++ 1353B.cpp
@@ -0,0 +1,61 @@
+//
+//  24/01/2024
+//  
+//  @itachi52730
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        int b[n];
+        vector<int> c;
+        for(int i = 0; i<n ;i++)
+        {
+            cin>>a[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            cin>>b[i];
+        }
+
+        sort(a,a+n);
+        sort(b,b+n);
+
+        for(int i = 0; i < k ; i++)
+        {
+            if(b[i] > a[i] )
+            {
+                c.push_back(b[n-i-1]);
+            }
+            else
+            {
+                c.push_back(a[i]);
+            }
+        }
+
+        int sum = 0;
+
+        for(int i = k ; i < n ; i++)
+        {
+           c.push_back(a[i]);
+        }
+
+        for(int i =0 ;i<n ;i++)
+        {
+            sum += c[i];
+        }
+
+        cout<<sum <<endl;
+        
+        
+    }
+    return 0;
+}
--- 227B.cpp
+++ 227B.cpp
@@ -5,13 +5,60 @@
 //
 #include<bits/stdc++.h>
 using namespace std;
+
+
+int bns(int n,int *a, int *b,int p)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid;
+    while (s<=e)
+    {
+        mid = (s+e) /2;
+
+        if(p > a[mid])
+        {
+            s = mid + 1;
+        }
+
+        else if(p < a[mid])
+        {
+            e = mid - 1;
+        }
+
+        else if(p == a[mid])
+        {
+            mid++;
+        }
+        mid = e-((e-s)/2);
+    }
+
+    return mid;
+}
+
+
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
+    int n;
+    cin>>n;
+    int a[n];
+    int vasya = 0, petya = 0; 
+    for (int i = 0; i < n; i++)
     {
-        
+        cin>>a[i];
     }
+
+    int m;
+    cin >> m;
+    int b[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >>b[i];
+        int p = bns(n,a,b,b[i]);
+        vasya += vasya + p;
+        petya += petya + (n-p+1);
+    }
+
+    cout<<vasya << " "<< petya <<endl;
     return 0;
 }
