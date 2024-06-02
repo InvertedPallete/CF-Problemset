cc64eaf646fc1ec4aa184aa6c6ccaa7e2e7b71ca
InvertedPallete <itachi52730@gmail.com>
2024-06-02 20:05:19 +0530

    2nd june

---
+++ 556A.cpp
@@ -0,0 +1,22 @@
+//
+//
+//  14/04/2024
+//  
+//  code by Gunjit Dhakar
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f(i,n) for (int i = 0; i < n; i++)
+
+int main()
+{
+    int t;
+    cin >>t;
+    while(t--)
+    {
+        
+    }
+    
+    return 0;
+}
