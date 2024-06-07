e6b56c19df527f29145301414bedbda92db29d3d
InvertedPallete <itachi52730@gmail.com>
2024-06-08 01:22:40 +0530

    8th of june 2024

--- 1348B.cpp
+++ 1348B.cpp
@@ -13,13 +13,36 @@ using namespace std;
 
 void solve()
 {   
-        
+        int N,K;
+    cin>>N>>K;
+    set<int>s;
+    for (int i=0;i<N;i++)
+    {
+        int a;
+        cin>>a;
+        s.insert(a);
+    }
+    if (s.size()>K)
+    {
+        cout<<"-1"<<endl;
+        return;
+    }
+    cout<<N*K<<endl;
+    for (int i=0;i<N;i++)
+    {
+        for (int b:s)
+        cout<<b<<' ';
+        for (int j=0;j<K-(int)s.size();j++)
+        cout<<1<<' ';
+    }
+    
+    cout << "\n";
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
-    int t;
+    ll int t;
     cin >> t;
 
     while(t--)
